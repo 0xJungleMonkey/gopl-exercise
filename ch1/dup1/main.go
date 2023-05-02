75171c222fb823473510db2cba93212a86fc51a1
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-05-02 15:04:19 -0400

    Ctrl D EOF or set up exit condition

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -16,12 +16,14 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	// 短变量声明创建bufio.Scanner类型的变量input.该变量从程序的标准输入中读取内容。每次调用input.Text()得到。Scan 函数在读到一行时返回true，不再有输入时返回false
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		// set up the end point for the user input
-		if input.Text() == "exit" {
-			break
-		}
+		// or client can do ctrl + D is EOF end of input.
+		// if input.Text() == "exit" {
+		// 	break
+		// }
 		counts[input.Text()]++
 	}
 	// NOTE: ignoring potential errors from input.Err()
