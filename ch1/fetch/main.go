c233f7739776a750d0f4cd507f4837423a8d8626
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-05-05 12:19:59 -0400

    获取单个URL

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -13,29 +13,27 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
-
+		//////exercise1.8
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
 		//////exercise1.7 函数调用io.Copy(dst, src)会从src中读取内容，并将读到的结果写入到dst中， 使用这个函数代替掉例子中的ioutil.ReadAll 来拷贝响应结构体到os.Stdout, 避免申请一个缓冲区来存储。 记得处理io.Copy返回结果中的错误。
-		if _, err := io.Copy((os.Stdout), resp); err != nil {
+		if _, err := io.Copy((os.Stdout), resp.Body); err != nil {
 			log.Fatal(err)
 		}
+		//////exericise1.9 打印HTTP 协议的状态码
+		fmt.Printf("\n%s\n", resp.Status)
 
-		////// Original
-		// b, err := ioutil.ReadAll(resp.Body)
-		// resp.Body.Close()
-		// if err != nil {
-		// 	fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-		// 	os.Exit(1)
-		// }
-		// fmt.Printf("%s", b)
 	}
 }
 
